153c5fb0e4c975ea9fbd3803111c8ff14cde1cc7
Kayla Martinez <kaylamartinez@kaylas-mbp.lan>
2023-05-11 11:18:45 -0400

    installed axios and created PlantList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PlantList from './components/PlantList';
 
 function App() {
   return (
     <div className="App">
-    <h1>HEY HEY</h1>
+    <PlantList />
     </div>
   );
 }
